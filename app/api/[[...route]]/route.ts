f6d95b1a1b799e867984b43ac796dde3f1db4688
lautaromateol <lautaromateol@gmail.com>
2024-11-26 12:41:55 -0300

    Hono.js configurado

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello Next"
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
+export const PATCH = handle(app)
+export const DELETE = handle(app)
