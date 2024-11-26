869f394d243b70228701753f5f7a41bf9816a3c8
lautaromateol <lautaromateol@gmail.com>
2024-11-26 14:05:21 -0300

    Colores personalizados añadidos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        shade: "#e3fafc",
+        main: "#99e9f2",
+        tint: "#0b7285"
       },
     },
   },
