3b0433123a1e3d16f47201adbeccbe47176e6c79
lautaromateol <lautaromateol@gmail.com>
2024-11-27 21:14:48 -0300

    Columnas Account

---
+++ app/dashboard/accounts/columns.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InferResponseType } from "hono"
+import { ColumnDef } from "@tanstack/react-table"
+import { AccountDropdown } from "@/features/accounts/components/account-dropdown"
+import { client } from "@/lib/client"
+import { Checkbox } from "@/components/ui/checkbox"
+
+type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0]
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: "Name"
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const account = row.original
+
+      return (
+        <AccountDropdown id={account.id} />
+      )
+    }
+  }
+]
