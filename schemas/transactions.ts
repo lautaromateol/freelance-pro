19ce9ad63cdbb0cd4872db4c2a32b990b9bfbd44
lautaromateol <lautaromateol@gmail.com>
2024-11-29 20:05:11 -0300

    Metodo nullable añadido a propiedades de esquema

--- schemas/transactions.ts
+++ schemas/transactions.ts
@@ -1,21 +1,21 @@
 import { z } from "zod";
 
 export const transactionApiSchema = z.object({
-  notes: z.string().optional(),
+  notes: z.string().optional().nullable(),
   payee: z.string(),
   amount: z.number(),
   date: z.coerce.date(),
-  categoryId: z.string().optional(),
+  categoryId: z.string().optional().nullable(),
   accountId: z.string(),
-  projectId: z.string().optional()
+  projectId: z.string().optional().nullable()
 })
 
 export const transactionSchema = z.object({
-  notes: z.string().optional(),
+  notes: z.string().optional().nullable(),
   payee: z.string(),
   amount: z.string(),
   date: z.coerce.date(),
-  categoryId: z.string().optional(),
+  categoryId: z.string().optional().nullable(),
   accountId: z.string(),
-  projectId: z.string().optional()
+  projectId: z.string().optional().nullable()
 })
