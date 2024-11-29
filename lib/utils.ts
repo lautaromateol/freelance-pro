f5668076e75c71661ad784faa6f25e7e3a77b71a
lautaromateol <lautaromateol@gmail.com>
2024-11-29 19:57:57 -0300

    Funciones helper para manipular amount

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertAmountToMilliunits(amount: number) {
+  return amount * 1000
+}
+
+export function convertAmountFromMilliunits(amount: number) {
+  return amount / 1000
+}
+
+export function convertToCurrency(amount: number) {
+  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount)
+}
