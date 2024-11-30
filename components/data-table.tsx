3ed471d7cb78971dff079ac3b39699257a1fcd9d
lautaromateol <lautaromateol@gmail.com>
2024-11-30 12:20:08 -0300

    Input de filtro de Data Table corregido

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -69,7 +69,7 @@ export function DataTable<TData, TValue>({
           placeholder={`Filter by ${filterKey}...`}
           value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn(filterKey)?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
