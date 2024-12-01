a3ec7a51038e3cc943bd77ab2bc395df67482bbd
lautaromateol <lautaromateol@gmail.com>
2024-12-01 19:07:03 -0300

    Pagina de Proyectos

---
+++ app/dashboard/projects/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+import { Loader2, PlusIcon } from "lucide-react";
+import { useNewProject } from "@/features/projects/hooks/use-new-project";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useDeleteProjects } from "@/features/projects/api/use-delete-projects";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/data-table";
+import { columns } from "./columns";
+
+export default function TransactionsPage() {
+
+  const { onOpen } = useNewProject()
+  const projectsQuery = useGetProjects()
+  const projects = projectsQuery.data || []
+
+  const { deleteProjects, isPending } = useDeleteProjects()
+
+  if (projectsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto">
+        <Card className="shadow-none">
+          <CardHeader>
+            <CardTitle>Your projects</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto">
+      <Card className="shadow-none">
+        <CardHeader>
+          <div className="flex flex-col lg:flex-row gap-y-1 lg:gap-y-0 items-center justify-between">
+            <CardTitle>Your projects</CardTitle>
+            <Button
+              className="flex items-center"
+              size="sm"
+              variant="outline"
+              onClick={onOpen}
+            >
+              <p className="text-sm font-medium">
+                Add Project
+              </p>
+              <PlusIcon className="size-4" />
+            </Button>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            data={projects}
+            filterKey="payee"
+            columns={columns}
+            onDelete={(rows, setRowsSelected) => {
+              const ids = rows.map((row) => row.original.id)
+              deleteProjects({ ids }, {
+                onSuccess: () => setRowsSelected({})
+              })
+            }}
+            disabled={isPending}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
