c6e7b824cca1869912a4413ad6182209a2eb98d0
lautaromateol <lautaromateol@gmail.com>
2024-12-01 18:58:46 -0300

    Dropdown de Proyecto v0

---
+++ features/projects/components/transaction-dropdown.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useOpenProject } from "@/features/projects/hooks/use-open-project";
+import { useDeleteProject } from "@/features/projects/api/use-delete-project";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+type Props = {
+  id: string
+}
+
+export function TransactionDropdown({ id }: Props) {
+
+  const { onOpen } = useOpenProject()
+
+  const { deleteProject, isPending } = useDeleteProject(id)
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="cursor-pointer" asChild>
+        <MoreHorizontal className="size-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem disabled={isPending} onClick={() => onOpen(id)}>
+          <div className="flex items-center gap-x-2">
+            Edit
+            <Edit className="size-4" />
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem disabled={isPending} onClick={() => deleteProject()}>
+          <div className="flex items-center gap-x-2">
+            Delete
+            <Trash className="size-4" />
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
