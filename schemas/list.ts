3a1a856d098e4324c8b25b25288e04ef06476fa6
lautaromateol <lautaromateol@gmail.com>
2024-12-05 21:02:23 -0300

    Esquema de actualización de Lista creado

--- schemas/list.ts
+++ schemas/list.ts
@@ -4,3 +4,8 @@ export const listSchema = z.object({
   name: z.string(),
   projectId: z.string()
 })
+
+export const listToUpdate = z.object({
+  name: z.string().optional(),
+  order: z.number().optional()
+})
