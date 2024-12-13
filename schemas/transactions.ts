8976babe8c6a454a57219d15121aa2a14253f039
lautaromateol <lautaromateol@gmail.com>
2024-12-13 14:28:51 -0300

    Validación para campo amount

--- schemas/transactions.ts
+++ schemas/transactions.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 export const transactionApiSchema = z.object({
   notes: z.string().optional().nullable(),
   payee: z.string(),
-  amount: z.number(),
+  amount: z.number().refine((data) => data !== 0, {
+    message: "Amount cannot be 0."
+  }),
   date: z.coerce.date(),
   categoryId: z.string().optional().nullable(),
   accountId: z.string(),
@@ -13,7 +15,9 @@ export const transactionApiSchema = z.object({
 export const transactionSchema = z.object({
   notes: z.string().optional().nullable(),
   payee: z.string(),
-  amount: z.string(),
+  amount: z.string().refine((data) => parseFloat(data) !== 0, {
+    message: "Amount cannot be 0."
+  }),
   date: z.coerce.date(),
   categoryId: z.string().optional().nullable(),
   accountId: z.string(),
