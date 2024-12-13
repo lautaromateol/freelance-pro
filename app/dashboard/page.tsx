7dedd5fa3db2cb79a01d41a09e89dd3fce981d6f
lautaromateol <lautaromateol@gmail.com>
2024-12-13 14:26:39 -0300

    Página de gráficos

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+import { DataGrid } from "@/features/dashboard/components/data-grid"
+import { DataCharts } from "@/features/dashboard/components/data-charts"
+
 export default function DashboardPage() {
+
   return (
-    <div>Protected route</div>
+    <div className="space-y-8">
+      <DataGrid />
+      <DataCharts />
+    </div>
   )
+
 }
