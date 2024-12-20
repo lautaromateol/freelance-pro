dd99178d8f95574af8bbac499928e551bcdaf9e2
lautaromateol <lautaromateol@gmail.com>
2024-12-19 22:05:17 -0300

    Renderizado condicional de botón en landing page

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,19 +1,24 @@
 "use client"
 import Link from "next/link";
 import { Logo } from "@/components/logo";
+import { useUser } from "@clerk/nextjs";
 
 export function NavBar() {
+
+  const { isLoaded, isSignedIn } = useUser()
 
   return (
     <>
       <header className="sticky top-0 bg-white flex items-center justify-between w-full h-14 px-4 py-6">
         <Logo />
         <ul className="md:flex items-center gap-x-4 text-sm font-medium text-main">
-          <Link href="/sign-in">
-            <li className="px-2 py-1 rounded-md bg-gradient-to-r from-main to-tint text-white hover:bg-tint">
-              Sign in
-            </li>
-          </Link>
+          {isLoaded && (
+            <Link href={`${isSignedIn ? "/dashboard" : "/sign-in"}`}>
+              <li className="px-2 py-1 rounded-md bg-gradient-to-r from-main to-tint text-white hover:bg-tint">
+                {isSignedIn ? "Dashboard" : "Sign in"}
+              </li>
+            </Link>
+          )}
         </ul>
       </header>
     </>
