594a0ff714793e7688dd120b91ec61c21edf133a
lautaromateol <lautaromateol@gmail.com>
2024-12-23 19:53:45 -0300

    Middleware actualizado para redirección post autenticación

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,32 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/api(.*)'])
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if(isProtectedRoute(req)) {
-    await auth.protect()
+  const { userId } = await auth();
+
+  if (userId && isPublicRoute(req)) {
+    const path = "/dashboard";
+
+    const pathUrl = new URL(path, req.url);
+    return NextResponse.redirect(pathUrl);
   }
-})
+
+  if (!userId && !isPublicRoute(req)) {
+    return (await auth()).redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
